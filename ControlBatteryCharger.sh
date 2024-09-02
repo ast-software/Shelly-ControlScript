66e1df9ef6462fe2de8d4102dbe3e94b06b14626
ast-software <ast_shopping@ptechnologies.org>
2024-09-02 09:31:38 +0200

    Add files via upload

--- ControlBatteryCharger.sh
+++ ControlBatteryCharger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# ControlBatteryCharger.sh
 #
 # Start/Stop Battery Charger
 #
@@ -16,23 +17,33 @@
 # Uncomment the following line to install on systems using apt-get
 #   apt-get install wget jq bc
 #
-# A Shelly PM1 plus can be read out by sending a hppt request with
+# Copy the script to a suitable location (e.g. /root/) and change file rights
+# to allow its execution as a shell script. For example,
+#
+# chmod a+rwx ControlBatteryCharger.sh
+#
+# Change the IP addresses of the Shellys in the script (see below) and test the script
+#
+#  ./ControlBatteryCharger 200
+#
+# How it works:
+# =============
+#
+# A Shelly (e.g. PM1 plus, Plug S) can be read out by sending a hppt request with
 # the rpc method 'Shelly.GetStatus'. Sending http requests can be easily 
-# done using the linu command wget
+# done using the linux command wget
 # Uncomment this line to see the output of a GetStatus call:
 #  content=$(wget 192.168.0.53/rpc/Shelly.GetStatus?id=0 -q -O -)
 # ("-O <file>" directs the downloaded file to <file>, "-O -" dumps it to standard output) 
 
-# A JSON object is returned which needs to be analyzed.
-# the linux command wget. JSON objects can be read out and analyzed using
+# A JSON object is returned which needs to be analyzed, which can be done using
 # the linux command jq.
 
-# An illustrative example how to fetch the value from the data field "name"
-# of a data structure "person":
+# Example: Fetch the value from the data field "name"
+# of a data structure "person" with fields "name" and "age":
 # echo '{"person":{"name":"John", "age":30}}' | jq '.person.name'
 
-# By using a pipe command, the output from wget is
-# used as input for jq. The Shelly's output has a data structure "switch:0"
+# The Shelly's output has a data structure "switch:0"
 # with several data fields for voltage, current, power etc. The power
 # is returned in the field 'apower'.
 
@@ -42,13 +53,34 @@
 # (repace turn=on by turn=off to switch the relay off)
 
 
-# An endless loop is implemented which fetches the current power of the PV
-# modules and sends a turn on or turn off command to the Shelly Plug S.
-# The power measurements are stored with a date and time stamp in the
+# The script implements 6 iterations (every 10 seconds) of checking solar power and
+# switching the charger on or off by sending a turn on or turn off command to the Shelly switch
+# controlling the battery charger.
+#
+# The measurements (power, current, voltage) are stored with a date and time stamp in the
 # textfile measurements.txt.
 
-# Turn on Battery Charger when Power exceeds bound
-bound=200
+# To start the script every minute invoke the command
+#   crontab -e
+# and add at the end of the crontab file the line
+# * * * * * /root/ControlBatteryCharger.sh 200
+# (Linux cron can not run a command regularly on a finer time scale!). 
+
+# EDIT THE FOLLOWING SETTINGS!
+# ============================
+
+# set IP addresses of the Shellys
+
+IPSHELLY_SOLAR=192.168.0.53
+IPSHELLY_CHARGER=192.168.0.182
+
+# Turn on Battery Charger when Power exceeds bound specified by 1st argument (default: 200)
+
+if [ $# -eq 0 ]; then
+  bound=200
+else
+  bound=$1
+fi
 
 # save PID to allow killing the process 
 echo $$ > ControlBatteryCharger.pid
@@ -56,40 +88,26 @@ echo $$ > ControlBatteryCharger.pid
 # Execute 6 times checking power and then wait 10s
 for i in {1..6}; 
 do
-  echo $i
+  #echo $i
   #power=$(wget 192.168.0.53/rpc/Shelly.GetStatus?id=0 -q -O - | jq '."switch:0".apower')
-  status=$(wget 192.168.0.53/rpc/Shelly.GetStatus?id=0 -q -O - )
+  status=$(wget "$IPSHELLY_SOLAR/rpc/Shelly.GetStatus?id=0" -q -O - )
   power=$(echo $status | jq '."switch:0".apower')
   voltage=$(echo $status | jq '."switch:0".voltage')
   current=$(echo $status | jq '."switch:0".current')
-  echo $(date) "Power: $power, Voltage: $voltage, Current: $current"
-  timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-  echo "$power > $bound" | bc 
   if [ "$(echo "$power > $bound" | bc)" = 1 ]; then
   #if (( $(echo "$power > $bound" |bc -l) )); then
-    echo "battery charger on"
-    #echo $(wget 192.168.0.182/rpc/Switch.GetStatus?id=0 -q -O - )
-    #echo $(wget 192.168.0.182/relay/0?turn=on -q -O - )
-    res=$(wget 192.168.0.182/relay/0?turn=on -q  -O - )
-    status=1
+    res=$(wget "$IPSHELLY_CHARGER/relay/0?turn=on" -q  -O - )
+    status=ON
   else
-    echo "battery charger off"
-    #echo $(wget 192.168.0.182/relay/0?turn=off -q -O - )
-    res=$(wget 192.168.0.182/relay/0?turn=off -q -O - )
-    status=0
+    res=$(wget "$IPSHELLY_CHARGER/relay/0?turn=off" -q -O - )
+    status=OFF
   fi
+  echo $(date) "Power: $power, Voltage: $voltage, Current: $current, STATUS: $status"
+  timestamp=$(date '+%Y-%m-%d %H:%M:%S')
   echo $timestamp $power $current $voltage >> messwerte.txt
-  echo $timestamp $power $current $voltage >> /media/Ansgar/Solardaten/messwerte.txt
-
-  # copy data file every minute 
-  # get current minute, compute remainder with respect to 3
-  # and copy the file if the remainder is 1
-  min=$(date '+%M')
-  min_mod_3=$(echo "scale=0; $min%3" | bc -l)
 
   # Wait until next 10s interval except in the last iteration
   if [ "$i" -le 5 ]; then
-    echo waiting $i
     startTime=$(date +%s)
     endTime=$(echo "($startTime/10+1) * 10" | bc)
     timeToWait=$(($endTime- $startTime))
